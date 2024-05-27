437a450fdb1459034e1d6fc3b4b87ef66ce31e44
Jhon Guevara <jhonguevara454@gmail.com>
2024-05-27 16:30:12 -0500

    primer compromiso

---
+++ Documents/EjercicioDelProyexto/JavaApplication83/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import main.java.serviceImpl.FabricaDeTrajes;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        FabricaDeTrajes.main(args);
+    }
+}
